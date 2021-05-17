16ac0e6780b8daa9359e681d3d6930ce1b2211f3
drepi <drepi@DESKTOP-5GOHKHI>
2021-05-17 14:38:59 +0900

    - 로컬 커밋 -

---
+++ hello_spring/src/main/java/kr/ac/inha/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package kr.ac.inha.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world!";
+	}
+	@RequestMapping("/inha")
+	public String inha(String param) {
+		return param+"inha.ac.kr";
+	}
+	@RequestMapping("/name")
+	public String name(String id) {
+		return "당신의 학번은 "+id+"입니다.";
+	}
+//	@RequestMapping("/num")
+//	public String num(String n) {
+//		for(int i=1;i<10;i++)
+//			return n+"*"+i+"="+String
+//	}
+}
